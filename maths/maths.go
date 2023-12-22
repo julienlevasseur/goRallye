b6342cdeb2a3bd8aa79a32c024661ccde1c47963
Julien Levasseur <julien.levasseur@free.fr>
2023-12-22 17:22:11 -0500

    fix: Typo

--- maths/maths.go
+++ maths/maths.go
@@ -9,17 +9,17 @@ import (
 // with number = 33.99 and precision = 2, Round returns 33.99
 // with number = 33.999 and precision = 2, Round returns 33.99
 // with number = 33.99 and precision = 3, Round returns 33.999
-func round(k float64) int {
+func Round(k float64) int {
   return int(k + math.Copysign(0.001, k))
 }
 
-// truncate return the given float64 value with a precision decimal
+// Truncate return the given float64 value with a precision decimal
 // It does not round it to .1, example:
 // with num = 1.123 & precision = 1, Truncate returns: 1.1
 // with num = 33.99 & precision = 2, Truncate returns: 33.99
 // with num = 33.99  & precision = 1, Truncate returns: 33.9
-func truncate(num float64, precision int) float64 {
+func Truncate(num float64, precision int) float64 {
 	output := math.Pow(10, float64(precision))
-	return float64(round(num*output)) / output
+	return float64(Round(num*output)) / output
 }
 
